bfc6084978c1203e0dccc7f256156f1cfab1c872
Shubham <shubhamtiwari4065@gmail.com>
2023-12-20 19:17:19 +0530

    Created Home page template

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+posts = [
+    {
+        "author": "Virat Kohli",
+        "title": "Centuries",
+        "content": "I have made 71 centuries",
+        "date_posted": "19th Dec, 2023",
+    },
+    {
+        "author": "Ravichandran Ashwin",
+        "title": "Wickets",
+        "content": "I have taken 500 wickets",
+        "date_posted": "20th Dec, 2023",
+    },
+]
+
 
 # Create your views here.
+def home(request):
+    context = {"posts": posts}
+    return render(request, "blog/home.html", context)
+
+
+def about(request):
+    return render(request, "blog/about.html", {"title": "About Page"})
