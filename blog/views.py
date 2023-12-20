0768688fc2dc8ae5afde5caac8487210ce7298a1
Shubham <shubhamtiwari4065@gmail.com>
2023-12-20 19:52:24 +0530

    Created database

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        "author": "Virat Kohli",
-        "title": "Centuries",
-        "content": "I have made 71 centuries",
-        "date_posted": "19th Dec, 2023",
-    },
-    {
-        "author": "Ravichandran Ashwin",
-        "title": "Wickets",
-        "content": "I have taken 500 wickets",
-        "date_posted": "20th Dec, 2023",
-    },
-]
+from .models import Post
 
 
 # Create your views here.
 def home(request):
-    context = {"posts": posts}
+    context = {"posts": Post.objects.all()}
     return render(request, "blog/home.html", context)
 
 
