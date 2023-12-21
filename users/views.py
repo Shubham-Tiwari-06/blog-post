eb80903e5eaf31026d8df07bc7bba9cc85d08e2a
Shubham <shubhamtiwari4065@gmail.com>
2023-12-21 13:26:52 +0530

    Developed CRUD operations

--- users/views.py
+++ users/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-from .forms import UserRegisterForm
 from django.contrib import messages
+from .forms import UserRegisterForm, UserUpdateForm
+from django.contrib.auth.decorators import login_required
 
 
-# Create your views here.
 def register(request):
     if request.method == "POST":
         form = UserRegisterForm(request.POST)
@@ -14,7 +14,28 @@ def register(request):
             messages.success(
                 request, f"Account Successfully Created for {username} Login In Now!!!"
             )
-            return redirect("blog-home")
+            return redirect("login")
     else:
         form = UserRegisterForm()
     return render(request, "users/register.html", {"form": form})
+
+
+def profile(request):
+    return render(request, "users/profile.html")
+
+
+@login_required
+def profile_update(request):
+    if request.method == "POST":
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+
+        if u_form.is_valid():
+            u_form.save()
+            messages.success(request, f"Your account has been updated!")
+            return redirect("profile")
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+
+    context = {"u_form": u_form}
+    return render(request, "users/profile_update.html", context)
